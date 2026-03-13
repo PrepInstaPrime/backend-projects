a631d2dea49bb3f3cace0ae08e25c0a0dab3b10f
Abhijit-Singh <abhijits1280@gmail.com>
2026-03-13 20:02:56 +0530

    Update route.mjs

--- bookmanagement/src/route.mjs
+++ bookmanagement/src/route.mjs
@@ -5,19 +5,24 @@ import { createReview, updateReview, deleteReview } from './controllers/reviewCo
 import { uploadImage } from './controllers/frontendController.mjs';
 import * as booksController from './controllers/bookControllers.mjs';
 const router = express.Router();
+
+// User routes
 router.post('/login', login);
 router.post('/register', register);
 
+// Book routes
 router.get('/books', verifyToken, booksController.getAll);
 router.get('/books/:id', verifyToken, booksController.getById);
 router.post('/books', verifyToken, booksController.add);
 router.put('/books/:id', verifyToken, booksController.update);
 router.delete('/books/:id', verifyToken, booksController.remove);
 
+// Review routes
 router.post('/books/:bookId/review', verifyToken, createReview);
 router.put('/books/:bookId/review/:reviewId', verifyToken, updateReview);
 router.delete('/books/:bookId/review/:reviewId',verifyToken, deleteReview);
 
+// File upload route
 router.post("/frontend",verifyToken ,uploadImage)
 
 export default router;
